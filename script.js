8601b62e44cdb173f5215e718d9dfc5a134cd5b7
algoritmo-entalpia <118137608+AlgoritmoEntalpia@users.noreply.github.com>
2022-11-14 21:19:52 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+/**
+ * 
+ * Autor: Gustavo da Silva Soares
+ * 
+ * Site: gustavo-soares-dev.github.io
+ * 
+ */
+
+ var at = false;
+
+ function change(){
+     if(at == true){
+         at = false;
+         document.getElementById("ca").style.opacity = 0;
+         document.getElementById("ca").style.width = "0px";
+     } else{
+         at = true;
+         document.getElementById("ca").style.opacity = 1;
+         document.getElementById("ca").style.width = "110px";
+     }
+ }
+ 
+ function calcular(){
+     var re = document.getElementById("reagentes").value;
+     var pr = document.getElementById("produtos").value; 
+     var ca = document.getElementById("ca").value;
+     var ent = pr - re;
+     document.getElementById("reagentes").value = "";
+     document.getElementById("produtos").value = "";
+     document.getElementById("ca").value = "";
+ 
+     if(at == true && ca == ""){
+         alert("Insira a energia do complexo ativado!");
+         return;
+     }
+ 
+ 
+     if(isNaN(ent) == true){
+         alert("Insira apensa números");
+         return;
+     }
+     if(pr == re){
+         alert("Essa reação não existe!");
+         return;
+     }
+ 
+      re>pr?document.getElementById("res1").innerHTML = "A reação é exotérmica":document.getElementById("res1").innerHTML = "A reação é endotérmica";
+ 
+      document.getElementById("res2").innerHTML = "A variação de entalpia é de " + ent;
+ 
+     if(at == true){
+         var eat = ca - re;
+         if(isNaN(eat) == true){
+             alert("Insira apensa números!");
+             return;
+         }
+         document.getElementById("res3").innerHTML = "A energia de ativação é " + eat;
+ 
+     }
+ 
+     }
+ 
